20a4d4d0f61efb8fd1eb920e28ce40c5556ce6b9
donghyeon <dhjoo98@hanmail.net>
2021-02-13 23:14:12 +0900

    alteration to fit current site

--- nike.py
+++ nike.py
@@ -1,12 +1,100 @@
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import ElementClickInterceptedException
 from selenium import webdriver
 import time
-driver = webdriver.Chrome('/Users/admin/Downloads/chromedriver')
+
+#remove webdriver flag
+option = webdriver.ChromeOptions()
+option.add_argument('--disable-blink-features=AutomationControlled')
+
+driver = webdriver.Chrome(executable_path='/Users/dhjoo/Desktop/Workspace/shopbot/chromedriver',options=option)
+#########큰 모니터에 풀스크린.
+#driver = webdriver.Safari()
+driver.get('https://www.nike.com/kr/launch/t/men/fw/basketball/CK5424-500/vvki54/zoom-freak-2')
+driver.get('https://www.nike.com/kr/launch/t/women/fw/nike-sportswear/CU1450-500/dlva36/w-nike-fontanka-edge')
+
+def login():
+    driver.find_element_by_xpath('//*[@id="jq_m_right_click"]/ul/li[1]/span/a[2]').click()
+    time.sleep(0.5)
+    driver.find_element_by_id('j_username').send_keys('ID_HERE')
+    time.sleep(0.5)
+    driver.find_element_by_id('j_password').send_keys('PW_HERE')
+    time.sleep(0.5)
+    driver.find_element_by_xpath('//*[@id="common-modal"]/div/div/div/div[2]/div/div[2]/div/button').click()
+
+login()
+##########꼭 로그인 미리 하시고!!
+timeout = 0.01
+shoeURL = 'https://www.nike.com/kr/launch/t/adult-unisex/fw/basketball/DH9888-600/usbo63/kobe-vi-protro'
+#페이지 로드 직후 버튼만 이 함수 쓰면 된다.
+def donghyeon_click(element, pause = timeout):
+    while True:
+        try:
+            driver.find_element_by_xpath(element).click()
+            break
+        except NoSuchElementException:
+            time.sleep(timeout)
+            #print('못찾겠다')
+            donghyeon_click(element)
+            break
+        except ElementClickInterceptedException :
+            time.sleep(timeout)
+            #print('못찾겠다')
+            donghyeon_click(element)
+            break
+
+def GimmeDatShoe_ver_recursive():
+    start_time = time.time()
+    driver.get(shoeURL)
+    #time.sleep(timeout)
+    donghyeon_click('/html/body/section/section/section/article/div/div[2]/div/div[3]/div[2]/div/div/form/div/div[1]/a')
+    #time.sleep(timeout)
+    # 사이즈 찾기
+    donghyeon_click('/html/body/section/section/section/article/div/div[2]/div/div[3]/div[2]/div/div/form/div/div[1]/ul/li[11]/a/span')
+    print('사이즈 선택 완료')
+    #donghyeon_click('/html/body/section/section/section/article/div/div[2]/div/div[3]/div[2]/div/div/form/div/div[1]/ul[text()='270']')
+    #time.sleep(timeout)
+    # 바로 구매 버튼 클릭
+    donghyeon_click('/html/body/section/section/section/article/div/div[2]/div/div[3]/div[2]/div/div/form/div/div[2]/div')
+    time.sleep(timeout)
+    print('다음버튼 누르는중')
+    donghyeon_click('// *[ @ id = "btn-next"]')
+    time.sleep(timeout)
+    print('kakao 찾는중')
+    donghyeon_click('//*[@id="payment-review"]/div[1]/ul/li[1]/div/div[1]/h6')
+    #time.sleep(timeout)
+    print('박스 찾는 중')
+    donghyeon_click('//*[@id="payment-review"]/div[1]/ul/li[2]/form/div/span/label/i')
+    #time.sleep(timeout)
+    donghyeon_click('//*[@id="complete_checkout"]/button', pause = timeout)
+    print('얼른 결제하자!!!')
+    print("---- %s seconds ----" % (time.time() - start_time))
+
+def GimmeDatShoe():
+    time.sleep(0.5)
+    driver.find_element_by_xpath('/html/body/section/section/section/article/div/div[2]/div/div[3]/div[2]/div/div/form/div/div[1]/a').click()
+    time.sleep(0.5)
+    # 사이즈 찾기
+    driver.find_element_by_xpath('/html/body/section/section/section/article/div/div[2]/div/div[3]/div[2]/div/div/form/div/div[1]/ul/li[11]/a/span').click()
+    time.sleep(0.5)
+    # 바로 구매 버튼 클릭
+    driver.find_element_by_xpath('/html/body/section/section/section/article/div/div[2]/div/div[3]/div[2]/div/div/form/div/div[2]/div').click()
+    time.sleep(0.5)
+    driver.find_element_by_xpath('// *[ @ id = "btn-next"]').click()
+    time.sleep(0.5)
+    driver.find_element_by_xpath('//*[@id="payment-review"]/div[1]/ul/li[1]/div/div[1]/h6').click()
+    time.sleep(0.5)
+    driver.find_element_by_xpath('//*[@id="payment-review"]/div[1]/ul/li[2]/form/div/span/label/i').click()
+    time.sleep(0.5)
+    driver.find_element_by_xpath('//*[@id="complete_checkout"]/button').click()
+
+GimmeDatShoe_ver_recursive()
+
 
 class Nike:
 
     def __init__(self):
-        self.login()
+        self.login() #로그인은 미리 해두기.
         self.findSizeInput()
         self.order()
         # self.payment()
@@ -14,48 +102,53 @@ class Nike:
     def login(self):
 
         # 쿠키 담기
-        cookies = "자신의 쿠키를 넣어주세요"
-
+        #cookies = {'domain':'.nike.com', 'name':'ActiveID', 'value':''}
+        '''
         driver.maximize_window()
         driver.implicitly_wait(3)
+        '''
         driver.get(
-            'https://www.nike.com/kr/ko_kr/t/adult-unisex/fw/football-soccer/AQ4174-001/fiuv99/superfly-7-elite-fg')
+            #'https://www.nike.com/kr/ko_kr/t/adult-unisex/fw/football-soccer/AQ4174-001/fiuv99/superfly-7-elite-fg'
+            'https://www.nike.com/kr/launch/t/men/fw/basketball/CK5424-500/vvki54/zoom-freak-2')
 
+        #driver.add_cookie(cookies)
+        #driver.refresh()
+
+        '''
         for cookie in cookies:
             driver.add_cookie(cookie)
-
-        driver.get(
-            'https://www.nike.com/kr/ko_kr/t/adult-unisex/fw/football-soccer/AQ4174-001/fiuv99/superfly-7-elite-fg')
+            '''
 
 
     def findSizeInput(self):
-
+        time.sleep(0.5)
+        driver.find_element_by_xpath('/html/body/section/section/section/article/div/div[2]/div/div[3]/div[2]/div/div/form/div/div[1]/a').click()
         time.sleep(0.5)
         # 사이즈 찾기
-        driver.find_element_by_xpath('/html/body/section/section/section/article/article[2]/div/div[4]/div/div[2]/form/div[1]/div[2]/div[1]/div/span[8]').click()
-
+        driver.find_element_by_xpath('/html/body/section/section/section/article/div/div[2]/div/div[3]/div[2]/div/div/form/div/div[1]/ul/li[11]/a/span').click()
         time.sleep(0.5)
         # 바로 구매 버튼 클릭
-        driver.find_element_by_xpath('//*[@id="btn-buy"]').click()
+        driver.find_element_by_xpath('/html/body/section/section/section/article/div/div[2]/div/div[3]/div[2]/div/div/form/div/div[2]/div').click()
 
     def order(self):
+        #time.sleep(0.5)
+        #driver.find_element_by_xpath('//*[@id="shipping_info"]/div[1]/ul/li[3]/div/span/label/i').click()
         time.sleep(0.5)
         driver.find_element_by_xpath('// *[ @ id = "btn-next"]').click()
         time.sleep(0.5)
-        driver.find_element_by_xpath('//*[@id="payment-review"]/div[1]/ul/li[2]/form/div/span').click()
+        driver.find_element_by_xpath('//*[@id="payment-review"]/div[1]/ul/li[1]/div/div[1]/h6').click()
         time.sleep(0.5)
-        driver.find_element_by_xpath('// *[ @ id = "payment-review"] / div[1] / ul / li[1] / div / div[1] / h6').click()
+        driver.find_element_by_xpath('//*[@id="payment-review"]/div[1]/ul/li[2]/form/div/span/label/i').click()
         time.sleep(0.5)
-        driver.find_element_by_xpath('//*[@id="payment-review"]/div[1]/ul/li[2]/form/div/span/label/span').click()
-        time.sleep(3)
-        driver.implicitly_wait(3)
-        driver.find_element_by_xpath('//*[@id="complete_checkout"]').click()
+        driver.find_element_by_xpath('//*[@id="complete_checkout"]/button').click()
 
+        '''
         # 결제
         iframe = driver.find_element_by_xpath("/html/body/div[13]/iframe[2]")
         driver.switch_to.frame(iframe)
         driver.find_element_by_xpath('/html/body/div[4]/div/div/div[2]/ul/li[2]/a').click()
         driver.find_element_by_id('userPhone').send_keys('01056741111')
+        '''
 
 
 if __name__ == "__main__":
